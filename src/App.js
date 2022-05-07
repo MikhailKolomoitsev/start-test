67d1053ae9f34e18855ec5269eee06a906a61842
Mihon <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-05-07 21:54:14 +0000

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Select from "./components/Select";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Select></Select>
       </header>
     </div>
   );
