26f28abf25149f9feaab55eeebad53d02e52d5ca
Mihon <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-05-10 12:51:04 +0000

    +

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React from 'react';
 import Form from './components/Form';
 
 function App() {
